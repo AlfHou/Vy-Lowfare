108c943664a0cf5cc7d523a8917f2fa1baf09c4b
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2019-11-29 17:05:15 +0100

    Set up endpoint for getting prices

---
+++ Backend/Controllers/JourneyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JourneyController : ControllerBase
+    {
+        private readonly VyService _vyService;
+
+        public JourneyController(VyService vyService)
+        {
+            _vyService = vyService;
+        }
+        [HttpGet]
+        public IEnumerable<int> Get(DateTime date)
+        {
+            System.Console.WriteLine(date);
+            return _vyService.GetPrices(date);
+        }
+    }
+}
---
+++ Backend/Services/VyService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Backend.Services
+{
+    public class VyService
+    {
+        public IEnumerable<int> GetPrices(DateTime date)
+        {
+            return new List<int>() { 10 };
+
+
+        }
+
+    }
+
+}
