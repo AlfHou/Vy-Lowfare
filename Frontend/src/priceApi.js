e016683a19f01c4639b32eb0dd28c6be9452e423
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2019-12-11 13:28:00 +0100

    Fix timezone error

--- Frontend/src/priceApi.js
+++ Frontend/src/priceApi.js
@@ -1,11 +1,14 @@
 const axios = require("axios").default;
 
 const instance = axios.create({
-    baseURL: "https://localhost:5001",
+    baseURL: "http://localhost:5001",
     timeout: 45000
 })
 
 function getPrices(date, to, from) {
+    // Have to adjust to take timezone into account...
+    let hoursDiff = date.getHours() - date.getTimezoneOffset() / 60;
+    date.setHours(hoursDiff);
     return instance.get("/journey", {
         params: {
             date: date,
