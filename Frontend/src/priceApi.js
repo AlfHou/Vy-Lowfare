028d5e1ce981cc635258aa45d97024e1a9fd3e2e
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2019-12-28 18:11:30 +0100

    Add small comment about next step todo

--- Frontend/src/priceApi.js
+++ Frontend/src/priceApi.js
@@ -4,6 +4,7 @@ const instance = axios.create({
     baseURL: "http://localhost:5000",
     timeout: 45000
 })
+// TODO: Have NGINX proxy all traffic from /api to the correct docker port
 
 function getPrices(date, to, from) {
     // Have to adjust to take timezone into account...
